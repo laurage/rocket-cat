1c13249566d5036bd203957e52e2a1e4a86db391
Laura Pedroni <laurage_@hotmail.com>
2016-09-02 18:12:23 +0200

    images redimensionned when width screen changes

--- sketch.js
+++ sketch.js
@@ -1,13 +1,14 @@
-var img;
 var image_x = 0;
-var image_y = 1;
+var image_y = 0;
 var number_img = 4;
+var originalCanvasWidth;
+var originalCanvasHeight;
 
 var img_list =[];
 
 var i = 0;
 
-// How long until the next image
+// Time from beginning + time to wait for next image
 var next = 0;
 
 function preload() {
@@ -18,27 +19,46 @@ function preload() {
 }
 
 function setup() {
+  // Stretches canvas to full window
   createCanvas(windowWidth, windowHeight);
+
+  originalCanvasWidth = windowWidth;
+  originalCanvasHeight = windowHeight;
+
+
 }
+
+// To resize canvas
+//function windowResized() { resizeCanvas(windowWidth, windowHeight); }
 
 function draw() {
   background(0, 100, 200);
 
-  //for (var i = 0; i<=3; i++){
-  displayImage();
+    displayImage();
+
+    // Timer
     if (millis() > next) {
+
       i++;
+
       if (i==number_img){
         i = 0;
       }
 
       next = millis() + 2000;
     }
-  //}
 }
 
 function displayImage(){
-  image(img_list[i], image_x, image_y);
-  console.log(i);
+  image(img_list[i], image_x, image_y, scaling(img_list[i]).x, scaling(img_list[i]).y);
 }
 
+// Scales images width & height proportionnally to the widht of the screen
+// img.width & img.height are the width and height of the original file. Doesn't change when redimensionned
+function scaling(img){
+  scaled_img_width = (windowWidth*img.width)/originalCanvasWidth;
+  scaled_img_height = (scaled_img_width*img.height)/img.width;
+  var img_size = createVector(scaled_img_width,scaled_img_height);
+
+  return img_size; //returns vector(width, height, 0)
+}
